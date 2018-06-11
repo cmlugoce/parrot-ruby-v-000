fceb2051a714cf46f4f9d6a95153401282ba0430
Cristina Lugo <cmlugoce@syr.edu>
2018-06-11 16:52:14 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot
-  puts "Squwak!"
-   "Squwak!"
-end
+  parrot = "Squwak!"
+  return "Squwak!"
+   
+parrot
